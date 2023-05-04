6f938fca1c21808900e6169e68f73748e8f8a858
y1nglun <1149587364@qq.com>
2023-05-04 14:49:46 +0800

    day_4_2

---
+++ day_4/day_4_5.py
@@ -0,0 +1,26 @@
+from day_4_4 import AnonymousSurvey
+import unittest
+
+
+class TestAnonymousSurveyCase(unittest.TestCase):
+    def setUp(self):
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
+    def test_store_single_response(self):
+        self.my_survey.store_response(self.responses[0])
+
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_response(self):
+
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+
+if __name__ == '__main__':
+    unittest.main()
